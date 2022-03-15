4020158e0d025d247ef33cab8b5eb97e050fa46d
tando96 <donhattan9a10@gmail.com>
2022-03-15 13:35:25 +0700

    Up Card CategoryPortfolio

--- containers/Project/index.js
+++ containers/Project/index.js
@@ -5,7 +5,30 @@ const Project = () => {
 	return (
 		<Stack spacing={2} direction='column'>
 			<PortfolioCard />
-			<CategoryPortfolioCard />
+			<CategoryPortfolioCard
+				id='1'
+				title='2D Projects'
+				subTitle='Artwork, Graphic Design, Photography'
+				imageSrc='/hover1.svg'
+			/>
+			<CategoryPortfolioCard
+				id='2'
+				title='3D Projects'
+				subTitle='Cinematic, Character, Artwork'
+				imageSrc='/hover2.svg'
+			/>
+			<CategoryPortfolioCard
+				id='3'
+				title='Visual Effects'
+				subTitle='TVC, Viral Videos, MV'
+				imageSrc='/hover3.svg'
+			/>
+			<CategoryPortfolioCard
+				id='4'
+				title='UX/UI Design'
+				subTitle='Application, Website, Game.'
+				imageSrc='/hover4.svg'
+			/>
 		</Stack>
 	);
 };
